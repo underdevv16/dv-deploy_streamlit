7306877862019429f4d1e182184413e1bd801507
Dev Vidit <124691886+underdevv16@users.noreply.github.com>
2025-01-22 14:28:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 Machine Learning App')
 
 # st.write('Hello world!')
 st.info('This app builds a machine learning model!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
