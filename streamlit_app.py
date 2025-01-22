e428ca7a9788912ded2d7b47385bf6573b2f57f6
Dev Vidit <124691886+underdevv16@users.noreply.github.com>
2025-01-22 14:34:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('🤖 Machine Learning App')
 # st.write('Hello world!')
 st.info('This app builds a machine learning model!')
 
-with st.expander('Data'):
+with st.expander("Data:"):
   st.write('**Raw Data:**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
