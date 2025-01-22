5084954a6a6f294a80c059fba3b4245ade9a89cd
Dev Vidit <124691886+underdevv16@users.noreply.github.com>
2025-01-22 14:22:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🤖 Machine Learning App')
 
-st.write('Hello world!')
+# st.write('Hello world!')
+st.info('This app builds a machine learning model!')
